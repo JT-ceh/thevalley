ddf7f3fde5765b6a36fc58500a9e73f908580123
The_JT <67649495+JT-ceh@users.noreply.github.com>
2023-02-27 09:01:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,45 +9,54 @@ app.use(cors());
 app.use(bodyParser.json());
 
 async function start(){
-  const client = new MongoClient("mongodb+srv://myself:self@cluster0.dbh8pow.mongodb.net/?retryWrites=true&w=majority");
+  const client = new MongoClient("mongodb+srv://vee:veelah@veela.ws8emjn.mongodb.net/?retryWrites=true&w=majority");
   await client.connect();
-  db=client.db("cailo");
+  db=client.db("thevalley");
   app.listen(3001,()=>console.log("Listening on 3001"));
 }
 start()
 
 //testing pinging server
-app.get('/',(req,res)=>{res.send("<b>Hello there, i'm up and running.</b>")})
-
-//tesing 
-app.get('/tt',(req,res)=>{res.send("<b>Hello there, testing.</b>")})
+app.get('/',(req,res)=>{res.send("<b>Hello there minds, i'm up and running.</b>")})
 
 //post attendance user
 app.post('/preg',function (req,res){
   const tcin=req.body.cin;
-  if(!tcin){db.collection('attendance').updateOne({name:req.body.name},{$set:{dtime:req.body.utime,cout:req.body.cout}},function(err,res){if(err)throw err;console.log("1 updated well")})}
+  if(!tcin){db.collection('users').updateOne({name:req.body.name},{$set:{dtime:req.body.utime,cout:req.body.cout}},function(err,res){if(err)throw err;console.log("1 updated well")})}
   else{
     const myReg = {name:req.body.name,dept:req.body.dep,utime:req.body.utime,dtime:null,cin:req.body.cin,cout:req.body.cout};
-    db.collection('attendance').insertOne(myReg, function(err, res) {if(err)throw err;console.log("1 document inserted in attandance");});}
+    db.collection('users').insertOne(myReg, function(err, res) {if(err)throw err;console.log("1 document inserted in attandance");});}
 })
 
 //post concern from whisleblower
-app.post('/pcons',function (req,res){
+app.post('/pmobile',function (req,res){
   const myOb = {description: req.body.description, summary:req.body.summary,sconc:req.body.conc, eemail:req.body.emaill};
-  db.collection("concerns").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted Cons");});
+  db.collection("mobile").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted Cons");});
+})
+
+//post join user
+app.post('/pjoin',function (req,res){
+  const myOb = {description: req.body.description, summary:req.body.summary,sconc:req.body.conc, eemail:req.body.emaill};
+  db.collection("minds").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted Cons");});
 })
 
 //post concern from whisleblower
-app.post('/psug',function (req,res){
+app.post('/pai',function (req,res){
   const myOb = {name:req.body.name,dep:req.body.dep};
   console.log("Testing "+myOb.name+"and you are"+myOb.dep);
-  db.collection("suggestions").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted in Sug");});
+  db.collection("ai").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted in Sug");});
 })
 
 //post concern from whisleblower
-app.post('/ptruck',function (req,res){
+app.post('/pweb',function (req,res){
+  const web={name:req.body.name,addr:req.body.dep,minds:req.body.minds,minds1:req.body.minds1,minds2:req.body.minds2};
+  db.collection("web").insertOne(web, function(err, res) {if(err)throw err;console.log("1 document inserted in trucks");});
+})
+
+//post concern from whisleblower
+app.post('/pdesktop',function (req,res){
   const myTruck={name:req.body.name,addr:req.body.dep,size:req.body.utime,cin:req.body.cin,cout:req.body.cout};
-  db.collection("trucks").insertOne(myTruck, function(err, res) {if(err)throw err;console.log("1 document inserted in trucks");});
+  db.collection("desktop").insertOne(myTruck, function(err, res) {if(err)throw err;console.log("1 document inserted in trucks");});
 })
 
 //user querries
@@ -55,57 +64,29 @@ app.post('/ptruck',function (req,res){
 app.post('/ulogin',function(req,res){
   //db.collection("concerns").insertOne({name:"talent",lpas:"myself"}, function(err, res){if(err)throw err;console.log("1 document inserted");});
   const user={name: req.body.name, lpas:req.body.lpas};
-  db.collection('cailousers').find({name:user.name,lpas:user.lpas}).toArray(function(err,result){if(err)throw err;res.send({data:result?true:false});})
+  db.collection('users').find({name:user.name,lpas:user.lpas}).toArray(function(err,result){if(err)throw err;res.send({data:result?true:false});})
 })
 
 //get concerns from whisleblower
-app.get('/gcons',function(req,res){
-  db.collection("concerns").find({}).toArray(function(err, result){if(err)throw err;res.send(result);});
+app.get('/gweb',function(req,res){
+  db.collection("web").find({}).toArray(function(err, result){if(err)throw err;res.send(result);});
 })
 //get concerns from whisleblower
-app.get('/guser',function(req,res){
-  db.collection("cailousers").find({}).toArray(function(err, result){if(err)throw err;res.send(result);});
+app.get('/gmobile',function(req,res){
+  db.collection("mobile").find({}).toArray(function(err, result){if(err)throw err;res.send(result);});
 })
 
 //get attendance register users
-app.get('/greg', function (req, res) {
-  db.collection('attendance').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
+app.get('/gai', function (req, res) {
+  db.collection('ai').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
 })
 
 //get attendance register users
-app.get('/gsug', function (req, res) {
-  db.collection('suggestions').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
+app.get('/gdesktop', function (req, res) {
+  db.collection('desktop').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
 })
 
-//get trucks
-app.get('/gtruck',function(req,res){
-  db.collection('trucks').find({}).toArray(function(err,result){if(err)throw err;res.send(result);})
-})
-
-app.post('/veela',function(req,res){
-  const {greet,els,good}=require('./mind/da.json');
-  const moods=[greet];
-  const stop=["is","of","are","i","so","and","the","them"];const d=/\d/g;const char=/\W/g;let result=[];let found=[];let notfound=[];let values=req.body.value.split(' ');
-    function log(x){console.log(x)}; // for console debugging
-    values=values.filter(x=>!stop.includes(x));values=values.filter(xz=>!d.test(xz));values=values.filter(xy=>!char.test(xy));// remove stop words and numbers and characters
-    values=[...new Set(values)];// remove dublicates
-    values.map(x=>log(x));
-    moods.forEach((mood)=>{
-      values.forEach((x)=>{
-       // let x1=x.split('');x1=x1.filter(xa=>!char.test(xa));x1=x1.filter(xb=>!d.test(xb));x1=x1.join('')//remove numbers and special characters from value
-       const re=RegExp(mood.keys1);
-       if(x.search(re)!=-1 && re.test(x)){result=[...result,1];found=[...found,x]}// if found
-        else{result=[...result,0];notfound=[...notfound,x]}// if not found
-      });// loop through user input values
-      decide(mood,req.body.value);log(found);result=[];found=[];notfound=[];
-    });// loop through moods 
-  
-    function decide(m,value){ // fx to determine and predict the closest response
-      const fsize=result.length;result=result.filter(x=>x);const clen=result.length;// get full size=>remove 0(not found)=>get remaining length
-      if(clen>=0.5*fsize){log("Req: "+value+"\nSuccess rate: "+clen/fsize);res.send({rp:m.rp[Math.floor(Math.random()*m.rp.length)],sug:m.sug})}// if current size is more than fullsize/2 then correct
-      else{
-          if(clen>0.1*fsize){log("Req: "+value+"\nSuccess rate: "+clen/fsize);res.send({rp:m.rp[Math.floor(Math.random()*m.rp.length)],sug:m.sug})}//if at least one size is more than fullsize/2 then correct
-          else{log("Req: "+value+"Failed with: "+clen/fsize);res.send({rp:els.rp[Math.floor(Math.random()*els.rp.length)],sug:els.sug})}
-        }
-    }
+//get joins
+app.get('/gjoin', function (req, res) {
+  db.collection('desktop').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
 })
